6d89c5e85a6d8aa152f4cef41c64414692a3ea13
Silvia Vanessa <info@silviavanessa.dev>
2024-05-09 21:13:33 +0100

    more plugins

---
+++ lua/morpho/plugins/todo-comments.lua
@@ -0,0 +1,31 @@
+return {
+  "folke/todo-comments.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local todo_comments = require("todo-comments")
+
+    -- TODO:
+    -- this is so nice
+
+
+    -- HACK:
+    -- some hack is going one here
+    
+    --BUG:
+    --ups a new mtf buggg
+
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set("n", "]t", function()
+      todo_comments.jump_next()
+    end, { desc = "Next todo comment" })
+
+    keymap.set("n", "[t", function()
+      todo_comments.jump_prev()
+    end, { desc = "Previous todo comment" })
+
+    todo_comments.setup()
+  end,
+}
