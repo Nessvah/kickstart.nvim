58de7b9645aeacc2450b0038688c163ee778847f
Silvia Vanessa <info@silviavanessa.dev>
2024-05-10 15:08:18 +0100

    add some git integrations

--- lua/morpho/plugins/lsp/mason.lua
+++ lua/morpho/plugins/lsp/mason.lua
@@ -2,53 +2,59 @@
 -- that we need for the languages that we work for
 
 return {
-  "williamboman/mason.nvim",
+  'williamboman/mason.nvim',
   dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    'williamboman/mason-lspconfig.nvim',
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
   },
   config = function()
     -- import mason
-    local mason = require("mason")
+    local mason = require 'mason'
 
     -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
+    local mason_lspconfig = require 'mason-lspconfig'
 
-    local mason_tool_installer = require("mason-tool-installer")
+    local mason_tool_installer = require 'mason-tool-installer'
 
     -- enable mason and configure icons
-    mason.setup({
+    mason.setup {
       ui = {
         icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
+          package_installed = '✓',
+          package_pending = '➜',
+          package_uninstalled = '✗',
         },
       },
-    })
+    }
 
-    mason_lspconfig.setup({
+    mason_lspconfig.setup {
       -- list of servers for mason to install
       ensure_installed = {
-        "tsserver",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "lua_ls",
-        "graphql",
-        "emmet_ls",
-        "prismals",
-        "pyright",
+        'bashls',
+        'tsserver',
+        'html',
+        'cssls',
+        'tailwindcss',
+        'lua_ls',
+        'graphql',
+        'emmet_ls',
+        'prismals',
+        'pyright',
       },
-    })
+    }
 
-    mason_tool_installer.setup({
+    mason_tool_installer.setup {
       ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
+        'prettier', -- prettier formatter
+        'stylua', -- lua formatter
+        'isort', -- python formatter
+        'black', -- python formatter
+        'eslint_d',
+        'debugpy',
+        'mypy',
+        'pylint',
+        'flake8',
       },
-    })
+    }
   end,
 }
